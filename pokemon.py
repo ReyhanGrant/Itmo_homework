a09ffd7ef4d3073289b7dad194806ab09cddffe2
JakeInTheGround <valerii.gimpel@gmail.com>
2021-12-01 23:40:23 +0300

    Finished main exercise and 3rd
    mercy me please

--- pokemon.py
+++ pokemon.py
@@ -1,31 +1,41 @@
 import requests
-class Pokemon:
-    def __init__(self, id, name, height, weight):
-        self.id = id
-        self.name = name
-        self.height = height
-        self.weight = weight
+from dataclasses import dataclass
+@dataclass
+class BasePokemon:
+        name: str
         pass
-    def Pokename(self):
-        print(self.name)
-class BasePokemon(Pokemon):
-    def __init__(self, name):
-        Pokemon.__init__(self, name)
-        self.name=name
-        pass
+@dataclass
+class Pokemon(BasePokemon):
+    id: str
+    height: str
+    weight: str
+    pass
+
 class PokeApi():
-    def __init__(self):
-        pass
-    def get_pokemon(self, pokename):
+    @staticmethod
+    def get_pokemon(pokename):
         try:
             url = 'https://pokeapi.co/api/v2/pokemon/' + pokename.lower()
             r = requests.get(url).json()
         except:
             return print("There is some errors")
-        pokedex = Pokemon(str(r["id"]), r["name"], str(r["height"]), str(r["weight"]))
-        print ("Name: " + pokedex.name.upper())
-        print("Id: " + pokedex.id)
-        print("Height: " + pokedex.height)
-        print("Weight: " + pokedex.weight)
+        return Pokemon(str(r["name"]).capitalize() , str(r["id"]), str(r["height"]), str(r["weight"]))
+    @staticmethod
+    def get_all(get_name, state):
+        if state=="True":
+            print(PokeApi().get_pokemon(get_name))
+        else:
+            print(BasePokemon(PokeApi().get_pokemon(get_name).name))
 pokemon_name = input("Type pokemon id or pokemon name: ")
-PokeApi().get_pokemon(pokemon_name)
+state = input("Get all info?(True or False): ")
+PokeApi().get_all(pokemon_name, state)
+print("----------------------------")
+PokeApi().get_all("ditto", "True")
+poke_weight = 0
+for i in range(49):
+    number = i+1
+    pokemon=PokeApi().get_pokemon(str(number))
+    if int(pokemon.weight) > int(poke_weight):
+        pocket_monster = pokemon
+        poke_weight = int(pokemon.weight)
+print(pocket_monster.name + " is the fattest pokemon with weight is " + pocket_monster.weight)
